9ba1cf8072c4d6744bd0e8f8720bf9a645ece5f5
ClimenteA <climente.alin@gmail.com>
2023-04-08 16:31:14 +0300

    feat: created modals

--- public/scripts.js
+++ public/scripts.js
@@ -2,6 +2,7 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
+    // MENU
     let openMenu = document.getElementById("open-menu");
     let closeMenu = document.getElementById("close-menu");
     let menuLinks = document.getElementById("menu-links");
@@ -18,5 +19,44 @@ document.addEventListener("DOMContentLoaded", () => {
         openMenu.classList.remove("none");
     })
 
+    // VEZI DETALII MODALS
+    let consultatii = document.getElementById("consultatii");
+    let consultatiiModal = document.getElementById("consultatii-modal");
+    let esteticaDeparazitare = document.getElementById("estetica-deparazitare");
+    let esteticaDeparazitareModal = document.getElementById("estetica-deparazitare-modal");
+    let sterilizareChirurgie = document.getElementById("sterilizare-chirurgie");
+    let sterilizareChirurgieModal = document.getElementById("sterilizare-chirurgie-modal");
+    let overlayDark = document.getElementById("overlay-dark");
+    let closeModalItems = document.getElementsByClassName("close-modal");
+
+    function closeModal() {
+        window.scrollTo(0, 0);
+        document.body.classList.remove("overflow-hidden");
+        overlayDark.classList.add("none");
+        consultatiiModal.classList.add("none");
+        esteticaDeparazitareModal.classList.add("none");
+        sterilizareChirurgieModal.classList.add("none");
+    }
+
+    Array.from(closeModalItems).forEach(element => {
+        element.addEventListener("click", closeModal);
+    });
+
+    overlayDark.addEventListener("click", closeModal);
+
+    consultatii.addEventListener("click", () => {
+        window.scrollTo(0, 0);
+        document.body.classList.add("overflow-hidden");
+        overlayDark.classList.remove("none");
+        consultatiiModal.classList.remove("none");
+    });
+
+    esteticaDeparazitare.addEventListener("click", () => {
+
+    });
+
+    sterilizareChirurgie.addEventListener("click", () => {
+
+    });
 
 });
